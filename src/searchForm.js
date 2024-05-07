71a33b68499ba4c23ad3abc3a404d52410407113
Lucas <lucas.vergara@live.com>
2024-05-07 00:11:21 -0300

    update

--- src/searchForm.js
+++ src/searchForm.js
@@ -2,7 +2,7 @@ import get from './getElement.js'
 import presentDrinks from './presentDrinks.js'
 
 
-const baseURL = 'https://www.thecocktaildb.com/api/json/v1/1search.php?s=';
+const baseURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 const form = get('.search-form');
 const input = get('[name="drink"]');
@@ -10,6 +10,6 @@ const input = get('[name="drink"]');
 form.addEventListener('keyup', function (e){
     e.preventDefault();
     const value = input.value
-   if(!value) return;
-   presentDrinks(`${baseURL}${value}`);
+    if(!value) return;
+    presentDrinks(`${baseURL}${value}`);
 });
